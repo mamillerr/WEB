9520f1f5778f2ef9cadce9022bf6982f9763d97c
mamillerr <mamillerr17@gmail.com>
2023-11-08 09:01:53 +0300

    Make question listing active

--- app/askme/views.py
+++ app/askme/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from random_word import RandomWords
+import random
+
+r = RandomWords()
+
+QUESTIONS = [
+    {
+        'id': i,
+        'img': 'https://klike.net/uploads/posts/2023-01/1674365337_3-31.jpg',
+        'title': ' '.join([r.get_random_word() for _ in range(random.randint(2, 3))]) + '?',
+        'description': ' '.join([r.get_random_word() for _ in range(random.randint(10, 15))]) + '...',
+        'likes_num': random.randint(0, 100),
+        'answers_num': random.randint(0, 5),
+        'tags': [r.get_random_word() for _ in range(2)],
+    } for i in range(10)
+]
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'questions': QUESTIONS})
 
 
 def login(request):
